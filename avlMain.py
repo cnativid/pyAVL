b6a4f5795554752c4df2e05c0e330c140fa57138
Carlos Anthony Natividad <cnatividad@ucdavis.edu>
2023-04-23 11:13:28 -0700

    checkpoint

--- avlMain.py
+++ avlMain.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     planeName = 'STARTG01'
     AVLsp = pyAVL.AVL()
     AVLsp.loadPlane(planeName)
-    # AVLsp.loadMass(planeName)
+    AVLsp.loadMass(planeName)
 
     AVLsp.setAtmosphere()
     AVLsp.setVelocity(275*1.6878099)
     AVLsp.addInput('oper')
     AVLsp.addInput('C1')
     AVLsp.runAVL()
+
